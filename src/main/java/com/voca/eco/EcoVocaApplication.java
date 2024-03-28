4bfb71cfe5b07093ed9c94b93bb0252c8a73de4c
Song <mu9800mu12@naver.com>
2024-03-28 17:48:03 +0900

    Initial commit

---
+++ src/main/java/com/voca/eco/EcoVocaApplication.java
@@ -0,0 +1,13 @@
+package com.voca.eco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoVocaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoVocaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voca/eco/EcoVocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voca.eco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoVocaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
